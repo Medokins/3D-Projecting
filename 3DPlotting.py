7e5eff1dc52961af5e90c99b16db546c4035539f
Medokins <nikodemszwast@gmail.com>
2022-01-20 18:16:08 +0100

    add all three side, fix indexing errors

--- 3DPlotting.py
+++ 3DPlotting.py
@@ -2,14 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from transformData import convertToBinary
 
-name = "stairs"
-
-imgArray = convertToBinary(name)
+imgArray = convertToBinary("triangles")
 
 figure = plt.figure(facecolor='w')
 ax = plt.axes(projection ='3d')
 ax.invert_xaxis()
-
 ax.set_xlabel('X', linespacing=3.2)
 ax.set_ylabel('Y', linespacing=3.2)
 ax.set_zlabel('Z', linespacing=3.2)
@@ -30,24 +27,30 @@ for x in range(3):
     for y in range(3):
         if imgArray[x * spacing + start,y * spacing + start] == 0:
             for i in range(3):
-                point = [i, y, 2-y]
-                allpoints.remove(point)
+                point = [i, x, 2-y]
+                if point in allpoints:
+                    allpoints.remove(point)
 
-#if imArray[1, 1]  == 0: point A(x, y, z) = (0, 0, 2) & (1, 0, 2) & (2, 0, 2) doesn't exist
-#if imArray[6, 1]  == 0: point A(x, y, z) = (0, 1, 2) & (1, 1, 2) & (2, 1, 2) doesn't exist
-#if imArray[11, 1] == 0: point A(x, y, z) = (0, 2, 2) & (1, 2, 2) & (2, 2, 2) doesn't exist
-
-#if imArray[1, 6]  == 0: point A(x, y, z) = (0, 0, 1) & (1, 0, 1) & (2, 0, 1) doesn't exist
-#if imArray[6, 6]  == 0: point A(x, y, z) = (0, 1, 1) & (1, 1, 1) & (2, 1, 1) doesn't exist
-#if imArray[11, 6] == 0: point A(x, y, z) = (0, 2, 1) & (1, 2, 1) & (2, 2, 1) doesn't exist
-
-#if imArray[1, 11]  == 0: point A(x, y, z) = (0, 0, 0) & (1, 0, 0) & (2, 0, 0) doesn't exist
-#if imArray[6, 11]  == 0: point A(x, y, z) = (0, 1, 0) & (1, 1, 0) & (2, 1, 0) doesn't exist
-#if imArray[11, 11] == 0: point A(x, y, z) = (0, 2, 0) & (1, 2, 0) & (2, 2, 0) doesn't exist
 
 #second square, side view:
+for x in range(3):
+    for y in range(3):
+        if imgArray[x*spacing + 2*spacing + 3*start,y * spacing + start] == 0:
+            for i in range(3):
+                point = [y, i, 2-y]
+                if point in allpoints:
+                    allpoints.remove(point)
+                    
+
 
 #third square, view from above:
+for x in range(3):
+    for y in range(3):
+        if imgArray[x * spacing + start,y*spacing + 2*spacing + 3*start] == 0:
+            for i in range(3):
+                point = [x, y-2, i]
+                if point in allpoints:               
+                    allpoints.remove(point)
 
 X = []
 Y = []
